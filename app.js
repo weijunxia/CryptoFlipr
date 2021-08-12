53195fd5000368ab61677583675aa84d73faac89
Wei Jun Xia <weijunx1224@gmail.com>
2021-08-12 14:29:32 -0400

    added images

--- app.js
+++ app.js
@@ -2,15 +2,15 @@
 let cards = [...document.querySelectorAll('.card')]
 let deck = [...document.getElementsByClassName('deck')]
 let newGame = document.getElementById('newGame')
+let timer = document.querySelector('#timer')
 let flipCount = document.getElementById('flipCount')
 let flips = 0
-let timer = document.querySelector('#timer')
 let seconds = 0
 let interval
 let flippedCards = []
 
 // functions
-// Durstenfeld shuffle https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+// Fisher-Yates shuffle
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
@@ -24,18 +24,20 @@ function startGame() {
   cards = shuffleArray(cards)
   for (let i = 0; i < cards.length; i++) {
     deck.innerHTML = ''
-    cards[i].classList.remove('test')
     cards[i].classList.add('visible')
-    cards[i].classList.remove('disabled')
-    cards[i].classList.remove('match')
+    cards[i].classList.remove('flipped', 'disabled', 'match')
   }
-  cardEventListeners()
+  addCardEventListeners()
+  timer.innerHTML = 0
+  clearInterval(interval)
   // reset flip count
-  timer = 0
   flips = 0
+  flipCount.innerHTML = flips
 }
 
 function startTimer() {
+  timer.innerHTML = 0
+  clearInterval(timer)
   interval = setInterval(function () {
     timer.innerHTML = seconds + 's'
     seconds++
@@ -53,23 +55,28 @@ function flipCounter() {
 function flipCard() {
   flippedCards.push(this)
   flipCounter()
+  flippedCards[0].classList.add('disabled')
   if (flippedCards.length === 2) {
-    if (flippedCards[0].type === flippedCards[1].type) {
-      flippedCards[0].classList.add('match', 'disabled')
-      flippedCards[1].classList.add('match', 'disabled')
+    if (
+      flippedCards[0].dataset.framework === flippedCards[1].dataset.framework
+    ) {
+      flippedCards[0].classList.add('match', 'disabled', 'flipped')
+      flippedCards[1].classList.add('match', 'disabled', 'flipped')
       flippedCards[0].removeEventListener('click', flipCard)
       flippedCards[1].removeEventListener('click', flipCard)
       flippedCards = []
     } else {
-      unmatched()
+      flippedCards[0].classList.add()
+      flippedCards = []
     }
   }
   console.log(flippedCards)
 }
 
 // event listeners
-window.onload = cardEventListeners()
-function cardEventListeners() {
+window.onload = addCardEventListeners()
+
+function addCardEventListeners() {
   for (let i = 0; i < cards.length; i++) {
     cards[i].addEventListener('click', flipCard)
   }
