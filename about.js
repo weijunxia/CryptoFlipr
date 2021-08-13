7e5558080188f1a2f893658f1799bc67c3f58164
Wei Jun Xia <weijunx1224@gmail.com>
2021-08-13 02:58:31 -0400

    I have decided I no longer require semi-live coin prices

--- about.js
+++ about.js
@@ -1,1 +1,17 @@
 const CoinGecko = require('coingecko-api')
+
+const CoinGeckoClient = new CoinGecko()
+let button = document.getElementById('htmlbutton')
+button.addEventListener('click', async () => {
+  let data = await CoinGeckoClient.exchanges.fetchTickers('bitfinex', {
+    coin_ids: ['bitcoin']
+  })
+  var _coinList = {}
+  var _datacc = data.data.tickers.filter((t) => t.target == 'USD')
+  ;['BTC'].forEach((i) => {
+    var _temp = _datacc.filter((t) => t.base == i)
+    var _res = _temp.length == 0 ? [] : _temp[0]
+    _coinList[i] = _res.last
+  })
+})
+console.log(_coinList)
