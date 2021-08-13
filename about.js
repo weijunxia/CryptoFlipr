6657fc102415c193d00fff08da69ce6d6ce36f3f
Wei Jun Xia <weijunx1224@gmail.com>
2021-08-13 03:28:41 -0400

    added information about each product

--- about.js
+++ about.js
@@ -1,17 +1,15 @@
-const CoinGecko = require('coingecko-api')
-
-const CoinGeckoClient = new CoinGecko()
-let button = document.getElementById('htmlbutton')
-button.addEventListener('click', async () => {
-  let data = await CoinGeckoClient.exchanges.fetchTickers('bitfinex', {
-    coin_ids: ['bitcoin']
-  })
-  var _coinList = {}
-  var _datacc = data.data.tickers.filter((t) => t.target == 'USD')
-  ;['BTC'].forEach((i) => {
-    var _temp = _datacc.filter((t) => t.base == i)
-    var _res = _temp.length == 0 ? [] : _temp[0]
-    _coinList[i] = _res.last
+$(document).ready(function () {
+  $.ajax({
+    type: 'GET',
+    url: 'http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?',
+    contentType: 'application/json; charset=utf-8',
+    async: false,
+    dataType: 'json',
+    success: function (data, textStatus, jqXHR) {
+      var markup = data.parse.text['*']
+      var blurb = $('<div></div>').html(markup)
+      $('#article').html($(blurb).find('p'))
+    },
+    error: function (errorMessage) {}
   })
 })
-console.log(_coinList)
